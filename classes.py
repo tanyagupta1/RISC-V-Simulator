73ebd51e8cee7775ab27857d4c7c4fb7e4e7a9a8
tanyagupta1 <tanya19119@iiitd.ac.in>
2022-12-10 01:40:45 +0530

    pipelining implemented

--- classes.py
+++ classes.py
@@ -30,20 +30,36 @@ class Fetch():
     signals={'isImm':0,'isAdd':0,'isSub':0,'isAnd':0,'isOr':0,'isSLL':0,'isSRA':0,'isLW':0,'isST':0,'isBEQ':0,
 'isSTORENOC':0, 'isLOADNOC':0}
     clock =0
-    registers={'pc':0,'ir':0,'I':0,'rd':0,'rs1':0,'rs2':0,'immx':0}
+    registers={'ir':0,'I':0,'rd':0,'rs1':0,'rs2':0,'immx':0}
 
-    def run(self,instruction_memory):
-        PC = self.registers['pc']
+    def run(self,instruction_memory,pc):
+        print("FETCH")
+        print("signals")
+        print(self.signals)
+        print("registers")
+        print(self.registers)
+        PC = pc
         self.registers['ir'] = instruction_memory.read_memory(PC)
 
 class Decode():
     signals={'isImm':0,'isAdd':0,'isSub':0,'isAnd':0,'isOr':0,'isSLL':0,'isSRA':0,'isLW':0,'isST':0,'isBEQ':0,
 'isSTORENOC':0, 'isLOADNOC':0}
     clock =0
-    registers={'pc':0,'ir':0,'I':0,'rd':0,'rs1':0,'rs2':0,'immx':0}
+    registers={'ir':0,'I':0,'rd':0,'rs1':0,'rs2':0,'immx':0}
 
     def run(self,GPR):
+        
+        print("DECODE")
+        if(self.registers['ir']==0):
+            return False
         self.signals = dict.fromkeys(self.signals, 0)
+        print("signals")
+        print(self.signals)
+        print("registers")
+        print(self.registers)
+
+        if(False):
+            return True
         self.registers['rd'] = int(self.registers['ir'][20:25],2)
         self.registers['rs1'] = int(self.registers['ir'][12:17],2)
         self.registers['rs2'] = int(self.registers['ir'][7:12],2)
@@ -90,14 +106,23 @@ class Decode():
             if(self.registers['ir'][25:32]=='1111111'):
                 self.signals['isLOADNOC'] = 1
         
+        return False
+        
 class Execute():
     signals={'isImm':0,'isAdd':0,'isSub':0,'isAnd':0,'isOr':0,'isSLL':0,'isSRA':0,'isLW':0,'isST':0,'isBEQ':0,
 'isSTORENOC':0, 'isLOADNOC':0}
 
     clock =0
-    registers={'pc':0,'ir':0,'I':0,'rd':0,'rs1':0,'rs2':0,'immx':0,'mem_res':0,'res':0}
+    registers={'ir':0,'I':0,'rd':0,'rs1':0,'rs2':0,'immx':0,'mem_res':0,'res':0}
 
     def run(self,GPR):
+        print("EXECUTE")
+        if(self.registers['ir']==0):
+            return
+        print("signals")
+        print(self.signals)
+        print("registers")
+        print(self.registers)
         op1 = GPR.read_reg(self.registers['rs1'])
         op2 = GPR.read_reg(self.registers['rs2'])
         if(self.signals['isImm']):
@@ -119,9 +144,16 @@ class Memory():
     signals={'isImm':0,'isAdd':0,'isSub':0,'isAnd':0,'isOr':0,'isSLL':0,'isSRA':0,'isLW':0,'isST':0,'isBEQ':0,
 'isSTORENOC':0, 'isLOADNOC':0}
     clock =0
-    registers={'pc':0,'ir':0,'I':0,'rd':0,'rs1':0,'rs2':0,'immx':0,'mem_res':0,'res':0}
+    registers={'ir':0,'I':0,'rd':0,'rs1':0,'rs2':0,'immx':0,'mem_res':0,'res':0}
 
     def run(self,data_memory,GPR):
+        print("MEMORY")
+        if(self.registers['ir']==0):
+            return
+        print("signals")
+        print(self.signals)
+        print("registers")
+        print(self.registers)
         if(self.signals['isLW']):
             self.registers['mem_res'] = data_memory.read_memory(self.registers['res']>>2)
         if(self.signals['isST'] or self.signals['isLOADNOC']):
@@ -134,28 +166,37 @@ class Writeback():
     signals={'isImm':0,'isAdd':0,'isSub':0,'isAnd':0,'isOr':0,'isSLL':0,'isSRA':0,'isLW':0,'isST':0,'isBEQ':0,
 'isSTORENOC':0, 'isLOADNOC':0}
     clock =0
-    registers={'pc':0,'ir':0,'I':0,'rd':0,'rs1':0,'rs2':0,'immx':0,'mem_res':0,'res':0}
+    registers={'ir':0,'I':0,'rd':0,'rs1':0,'rs2':0,'immx':0,'mem_res':0,'res':0}
 
     def run(self,GPR):
+        print("WRITEBACK")
+        if(self.registers['ir']==0):
+            return
+        print("signals")
+        print(self.signals)
+        print("registers")
+        print(self.registers)
         if(self.signals['isLW']):
             GPR.write_reg(self.registers['rd'],self.registers['mem_res'])
         if(self.signals['isOr']or self.signals['isAnd'] or self.signals['isSLL'] or self.signals['isSRA'] or self.signals['isAdd'] or self.signals['isSub']):
             GPR.write_reg(self.registers['rd'],self.registers['res'])
-        if(self.signals['isBEQ']==1):
-            self.registers['pc'] = self.registers['pc']+self.registers['immx']
-        else:
-            self.registers['pc'] = self.registers['pc']+4
+        # if(self.signals['isBEQ']==1):
+        #     self.registers['pc'] = self.registers['pc']+self.registers['immx']
+        # else:
+        #     self.registers['pc'] = self.registers['pc']+4
 
 class CPU():
     clock=0
     instruction_memory ={}
     data_memory=[]
     GPR=[]
+    PC=0
     fetch_unit=0
     decode_unit=0
     execute_unit=0
     memory_unit=0
     writeback_unit=0
+    stall = False
 
 
     def __init__(self, im, dm, reg):
@@ -168,47 +209,64 @@ class CPU():
         self.memory_unit = Memory()
         self.writeback_unit= Writeback()
 
-    def fetch(self,instruction_memory):
-        self.fetch_unit.signals = self.writeback_unit.signals
-        self.fetch_unit.registers = self.writeback_unit.registers
-        self.fetch_unit.run(instruction_memory)
+    def fetch(self,instruction_memory,pc):
+        self.fetch_unit.run(instruction_memory,pc)
 
     def decode(self,GPR):
-        self.decode_unit.signals = self.fetch_unit.signals
-        self.decode_unit.registers = self.fetch_unit.registers
-        self.decode_unit.run(GPR)
+        self.stall  = self.decode_unit.run(GPR)
+        
 
     def execute(self,GPR):
-        self.execute_unit.signals=self.decode_unit.signals 
-        self.execute_unit.registers=self.decode_unit.registers 
         self.execute_unit.run(GPR)
+        
 
     def memory(self,data_memory,GPR):
-        self.memory_unit.signals = self.execute_unit.signals
-        self.memory_unit.registers = self.execute_unit.registers
         self.memory_unit.run(data_memory,GPR)
+        
     
     def writeback(self,GPR):
+        self.writeback_unit.run(GPR)
+        
+    
+    def transfer_sig(self):
         self.writeback_unit.signals = self.memory_unit.signals
         self.writeback_unit.registers = self.memory_unit.registers
-        self.writeback_unit.run(GPR)
+
+        self.memory_unit.signals = self.execute_unit.signals
+        self.memory_unit.registers = self.execute_unit.registers
+
+        self.execute_unit.signals=self.decode_unit.signals 
+        self.execute_unit.registers=self.decode_unit.registers 
+
+        if(not self.stall):
+            self.decode_unit.signals = self.fetch_unit.signals
+            self.decode_unit.registers = self.fetch_unit.registers
+
+
+
+
 
     def cpu_clock_edge(self):
+        print("Cycle no: ", self.clock)
         global file1
-        self.fetch(self.instruction_memory)
-        self.decode(self.GPR)
+        if(not self.stall):
+            self.fetch(self.instruction_memory,self.PC)
+            self.PC = self.PC+4
+            self.stall  = self.decode(self.GPR)
+        else:
+            print("Stalling on decode")
         self.execute(self.GPR)
         self.memory(self.data_memory,self.GPR)
         self.writeback(self.GPR)
+        self.transfer_sig()
 
-
-        print("Cycle no: ", self.clock)
-        print("signals")
-        print(self.writeback_unit.signals)
+        
+        # print("signals")
+        # print(self.writeback_unit.signals)
         print("GPR")
         print(self.GPR.GPR)
-        print("registers")
-        print(self.writeback_unit.registers)
+        # print("registers")
+        # print(self.writeback_unit.registers)
         print("data memory")
         for i in range(len(self.data_memory.data_memory)):
             if(self.data_memory.read_memory(i)!=0):
@@ -252,7 +310,7 @@ with open(sys.argv[1]) as file:
 for i in range(len(lines)):
     instruction_memory.write_memory(4*i,lines[i][0:32])
 
-while(my_cpu.fetch_unit.registers['pc']<4*len(lines)):
+while(my_cpu.PC<4*len(lines)):
     my_cpu.cpu_clock_edge()
 file1.write("Ending memory state: \n")
 for i in range(len(data_memory.data_memory)):
